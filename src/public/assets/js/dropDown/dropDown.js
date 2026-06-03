f3ac2fae1eb783c2bffc0de3b0226aabbfa9e2f6
CamilaSGom <camilasgomes00@gmail.com>
2026-06-03 14:42:09 -0300

    style: dropDown

--- src/public/assets/js/dropDown/dropDown.js
+++ src/public/assets/js/dropDown/dropDown.js
@@ -1,17 +1,20 @@
-function toggleDropdown() {
-  const dropdown = document.getElementById("userDropdown");
-  dropdown.style.display = dropdown.style.display === "flex" ? "none" : "flex";
+function toggleDropdownMenu() {
+  const dropdownButtonMenu = document.getElementById('dropdownButtonMenu');
+  const dropdownMenuContent = document.getElementById('dropdownMenuContent');
+
+  dropdownMenuContent.style.display = dropdownMenuContent.style.display === 'flex' ? 'none' : 'flex';
 }
 
 function toggleDropdownHistory() {
-  const dropDownHistory = document.getElementById("historyDropdown");
-  dropDownHistory.style.display = dropDownHistory.style.display === "flex" ? "none" : "flex";
+  const dropdownButton = document.getElementById('dropdownButton');
+  const dropdownHistoryContent = document.getElementById('dropdownHistoryContent');
+
+  dropdownHistoryContent.style.display = dropdownHistoryContent.style.display === 'flex' ? 'none' : 'flex';
 }
 
-// Fecha o dropdown se clicar fora
-// window.onclick = function(event) {
-//   const dropDown = document.getElementById("userDropdown");
-//   if (!event.target.closest('.user-icon') && !event.target.closest('#userDropdown')) {
-//     dropDown.style.display = "none";
-//   }
-// }
+window.onclick = function(event) {
+   const dropDown = document.getElementsByClassName("dropdown-menu");
+   if (!event.target.closest('.dropBtn') && !event.target.closest('.dropdown-menu')) {
+     dropDown.style.display = "none";
+   }
+ }
